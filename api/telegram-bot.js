1617fda7fa8526610c527867df903b95ebf9fe7d
rafsan3503 <rafsanchowdhuryrudro@gmail.com>
2023-02-22 19:41:10 +0600

    first commit

---
+++ api/telegram-bot.js
@@ -0,0 +1,12 @@
+const { Telegram } = require('telegraf');
+const telegraf = new Telegram(process.env.TELEGRAM_BOT_TOKEN);
+
+module.exports = async (req, res) => {
+  const { message } = req.body;
+  const chatId = message.chat.id;
+  const text = message.text;
+
+  await telegraf.sendMessage(chatId, `You said: ${text}`);
+  
+  res.status(200).send('OK');
+}
